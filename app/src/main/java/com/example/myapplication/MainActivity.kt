caa1f5539f2accfd7e427bb4a15f1d46782cbbb6
Gab2006 <tostigabriele6@gmail.com>
2025-04-10 09:53:12 +0200

    primo commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var btnRoll :Button = findViewById(R.id.buttonRoll)
+        btnRoll.setOnClickListener(View.OnClickListener {
+        var toast = Toast.makeText(this, "LANCIO DEL DADO", Toast.LENGTH_LONG)
+        toast.show()
+        var newIntent : Intent = Intent (this, SecondActivity::class.java)
+        var myRandom= (1..6).random()
+        newIntent.putExtra("Message", "NUMERO ESTRATTO: $myRandom")
+        startActivity(newIntent)
+        })
+    }
+}
